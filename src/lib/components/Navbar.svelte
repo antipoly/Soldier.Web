4ff07bd7261287f907a8f0b6a0bf0ee494d2b620
antipoly <80296087+antipoly@users.noreply.github.com>
2025-05-15 01:47:33 -0500

    first commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+</script>
+
+<nav class="flex items-center justify-between h-20 w-full pl-24 pr-10 py-5 bg-gradient-to-r from-[#295a9595] via-[#23869B95] to-[#3D67D395] nav-shadow">
+  <a href="/" aria-label="Logo button">
+    <div class="flex items-center gap-8">
+      <div class="w-[37px] h-[44px] bg-cover bg-no-repeat" style="background-image: url('/logo/Soldier@76x90.png');"></div>
+      <span class="text-title text-white tracking-wider font-bungee">SOLDIER</span>
+    </div>
+  </a>
+
+  <div class="flex items-center gap-12">
+
+  </div>
+</nav>
+
+<style>
+  .nav-shadow {
+    box-shadow: 0px 15px 49px 15px rgba(79, 71, 170, 0.25);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Navbar from "$lib/components/Navbar.svelte";
+  import '../app.css'
+
+	let { children } = $props();
+</script>
+
+<div class="backdrop">
+  <Navbar />
+
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  <div class="h-48 w-48"></div>
+  {@render children()}
+</div>
+
+<style>
+  .backdrop {
+    position: relative; /* Ensures content is positioned relative to the backdrop */
+    z-index: 1; /* Content stays above the backdrop */
+  }
+
+  .backdrop::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url('/Site.svg') no-repeat center center fixed;
+    background-size: cover;
+    z-index: -1;
+    opacity: 0.1;
+    pointer-events: none;
+  }
+</style>
